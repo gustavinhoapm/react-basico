b091fed6e8866b3df10df7ddab5651cee2014b65
gustavinhoapm <gustavo.agostinho2021@hotmail.com>
2023-03-10 11:39:45 -0300

    Curso React

---
+++ src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comentario.css'
+
+const Comentario = () => (
+    <div className="Comentario">
+    <div>Queria dizer apenas que:</div>
+    <div> estou nervoso com esse curso</div>
+    </div>
+);
+
+export default Comentario;
